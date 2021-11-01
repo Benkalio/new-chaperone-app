14d99f218e75c55d12f134b012f9679ac128acb9
Tennyson Ben-kalio <69404440+Benkalio@users.noreply.github.com>
2021-11-01 21:00:40 +0100

    keys

--- src/notes.js
+++ src/notes.js
@@ -12,7 +12,7 @@
       "Where files and softwares are kept in a computer system. Can Also be called a hard-disk or disk-drive."
   },
   {
-    key: 1,
+    key: 3,
     title: "Storage",
     content:
       "Where files and softwares are kept in a computer system. Can Also be called a hard-disk or disk-drive."
