61d71ecf047201ed3f3413c9d39cb974382a3eac
Tennyson Ben-kalio <69404440+Benkalio@users.noreply.github.com>
2021-11-01 20:59:06 +0100

    notes

---
+++ src/notes.js
@@ -0,0 +1,32 @@
+[
+  {
+    key: 1,
+    title: "Storage",
+    content:
+      "Where files and softwares are kept in a computer system. Can Also be called a hard-disk or disk-drive."
+  },
+  {
+    key: 2,
+    title: "Softwares",
+    content:
+      "Where files and softwares are kept in a computer system. Can Also be called a hard-disk or disk-drive."
+  },
+  {
+    key: 1,
+    title: "Storage",
+    content:
+      "Where files and softwares are kept in a computer system. Can Also be called a hard-disk or disk-drive."
+  },
+  {
+    key: 1,
+    title: "Storage",
+    content:
+      "Where files and softwares are kept in a computer system. Can Also be called a hard-disk or disk-drive."
+  },
+  {
+    key: 1,
+    title: "Storage",
+    content:
+      "Where files and softwares are kept in a computer system. Can Also be called a hard-disk or disk-drive."
+  }
+];
